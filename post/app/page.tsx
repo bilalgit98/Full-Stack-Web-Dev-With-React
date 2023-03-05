b78e1e04632d459174f0fac400b4d488a1818307
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2023-03-05 22:35:14 +0000

    Fetch Data -1

--- post/app/page.tsx
+++ post/app/page.tsx
@@ -1,10 +1,24 @@
 "use client";
+import axios from "axios";
 import CreatePost from "./components/AddPost";
+import { useQuery } from "@tanstack/react-query";
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPost");
+  return response.data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+  if (error) return error;
+  if (isLoading) return "Loading...";
+  console.log(data);
+
   return (
     <main>
-      <h1> Hello</h1>
+      <h1> Hello there 👋</h1>
       <CreatePost />
     </main>
   );
