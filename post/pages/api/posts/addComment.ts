b9b3068268add55924174c15ffb956ce932788d1
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2023-03-06 23:58:16 +0000

    AddComment -1

---
+++ post/pages/api/posts/addComment.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+import { authOption } from "../auth/[...nextauth]";
+import prisma from "../../../prisma/client";
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const session = await getServerSession(req, res, authOption);
+    if (!session) return res.status(401).json({ message: "Please sign in" });
+
+    const prismaUser = await prisma.user.findUnique({
+      where: {
+        email: session?.user?.email,
+      },
+    });
+    try {
+      const { title, postId } = req.body.data;
+      if (!title.length) {
+        return res.status(401).json({ message: "Please enter something!" });
+      }
+      const result = await prisma.comment.create({
+        data: {
+          message: title,
+          userId: prismaUser?.id,
+          postId,
+        },
+      });
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(403).json({ err: "Error has occurred, Please try again!" });
+    }
+  }
+}
