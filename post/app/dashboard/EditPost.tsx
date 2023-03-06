a8e58178cc99c6f368a5e0ce00361a7f0c1a4574
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2023-03-06 19:23:15 +0000

    Delete Post -1

--- post/app/dashboard/EditPost.tsx
+++ post/app/dashboard/EditPost.tsx
@@ -2,7 +2,9 @@
 
 import Image from "next/image";
 import { useState } from "react";
-
+import Toggle from "./Toggle";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 type EditProps = {
   id: string;
   avatar: string;
@@ -22,21 +24,50 @@ export default function EditPost({
   comments,
   id,
 }: EditProps) {
+  const [toggle, setToggle] = useState(false);
+
+  const { mutate } = useMutation(
+    async (id: string) =>
+      await axios.delete("/api/posts/deletePost", { data: id }),
+    {
+      onError: (error) => {
+        console.log(error);
+      },
+      onSuccess: (data) => {
+        console.log(data);
+      },
+    }
+  );
+
+  const deletePost = () => {
+    mutate(id);
+  };
   return (
-    <div className="bg-white my-8 p-8 rounded-lg">
-      <div>
-        <Image width={32} height={32} src={avatar} alt="avatar" />
-        <h3 className="font-bold text-gray-700">{name}</h3>
+    <>
+      <div className="bg-white my-8 p-8 rounded-lg">
+        <div>
+          <Image width={32} height={32} src={avatar} alt="avatar" />
+          <h3 className="font-bold text-gray-700">{name}</h3>
+        </div>
+        <div className="my-8">
+          <p className="break-all">{title}</p>
+        </div>
+        <div className="flex items-center gap-4">
+          <p className="text-sm font-bold text-gray-700">
+            {comments?.length} Comments
+          </p>
+          <button
+            onClick={(e) => {
+              setToggle(true);
+            }}
+            className="text-sm font-bold text-red-500"
+          >
+            Delete
+          </button>
+        </div>
       </div>
-      <div className="my-8">
-        <p className="break-all">{title}</p>
-      </div>
-      <div className="flex items-center gap-4">
-        <p className="text-sm font-bold text-gray-700">
-          {comments?.length} Comments
-        </p>
-        <button className="text-sm font-bold text-red-500">Delete</button>
-      </div>
-    </div>
+
+      {toggle && <Toggle deletePost={deletePost} setToggle={setToggle} />}
+    </>
   );
 }
